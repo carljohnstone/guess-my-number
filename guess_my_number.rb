3253814de8d70e0716964fe7541d5a3bfe4b8c49
Carl Johnstone <carl.johnstone@onthebeach.co.uk>
2013-09-23 22:44:38 +0100

    initial implementation

--- guess_my_number.rb
+++ guess_my_number.rb
@@ -12,3 +12,29 @@ def lower
   @guesser.lower
   @guesser.guess
 end
+
+class NumberGuesser
+
+  attr_accessor :highest_guess
+  attr_accessor :lowest_guess
+  attr_accessor :guess
+
+  def initialize
+    @lowest_possible = 1
+    @highest_possible = 100
+  end
+
+  def guess
+    @lowest_possible + (@highest_possible - @lowest_possible) / 2
+  end
+
+  def lower
+    @highest_possible = guess - 1
+  end
+
+  def higher
+    @lowest_possible = guess + 1
+  end
+
+end
+
